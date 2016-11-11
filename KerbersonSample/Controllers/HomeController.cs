f3cfb07ecebcc6868fcd687b94867da7b30b48d6
jbalessandro <jb.alessandro@gmail.com>
2016-11-11 17:40:54 -0200

    Add project files.

---
+++ KerbersonSample/Controllers/HomeController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using KerbersonSample.Models;
+
+namespace KerbersonSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private AgendaEntities db = new AgendaEntities();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(db.Agenda.ToList());
+        }
+
+        // GET: Home/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Agenda agenda = db.Agenda.Find(id);
+            if (agenda == null)
+            {
+                return HttpNotFound();
+            }
+            return View(agenda);
+        }
+
+        // GET: Home/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Home/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nome,Email")] Agenda agenda)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Agenda.Add(agenda);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(agenda);
+        }
+
+        // GET: Home/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Agenda agenda = db.Agenda.Find(id);
+            if (agenda == null)
+            {
+                return HttpNotFound();
+            }
+            return View(agenda);
+        }
+
+        // POST: Home/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nome,Email")] Agenda agenda)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(agenda).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(agenda);
+        }
+
+        // GET: Home/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Agenda agenda = db.Agenda.Find(id);
+            if (agenda == null)
+            {
+                return HttpNotFound();
+            }
+            return View(agenda);
+        }
+
+        // POST: Home/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Agenda agenda = db.Agenda.Find(id);
+            db.Agenda.Remove(agenda);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ KerbersonSample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KerbersonSample.MvcApplication" Language="C#" %>
